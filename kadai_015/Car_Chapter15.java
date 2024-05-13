d9c842be31ef276bf1cb140d74405b2a4428c53d
sakihamayuuka <166109381+sakihamayuuka@users.noreply.github.com>
2024-05-13 14:42:45 +0900

    kadai upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,12 @@
+
+package kadai_015;
+
+class  Car_Chapter15{ 
+	public static void main(String[] args) {
+    		CarExec_Chapter15 car1   = new CarExec_Chapter15();    		
+    		
+    		car1.gearChange(3);
+    		car1.run();
+    		
+    	    }
+}
