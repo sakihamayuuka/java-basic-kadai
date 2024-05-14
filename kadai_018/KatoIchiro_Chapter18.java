6671bc5d799063c1b18fc112af4c543c1dfb6f6d
sakihamayuuka <166109381+sakihamayuuka@users.noreply.github.com>
2024-05-14 15:41:31 +0900

    Update KatoIchiro_Chapter18.java

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -2,12 +2,12 @@ package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
 
-    public KatoIchiro_Chapter18() {
-        super("一郎");  // 親クラスのコンストラクタを呼び出してgivenNameをセット
+    public void setGivenName() {
+        givenName = "一郎";
     }
 
+
     // 個別の紹介を出力するメソッド
-    @Override
     public void eachIntroduce() {
         System.out.println("好きな食べ物はリンゴです");
     }
