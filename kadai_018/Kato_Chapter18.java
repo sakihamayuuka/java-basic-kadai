8589de843657f3dd22126633b20ee28c9ebf9a72
sakihamayuuka <166109381+sakihamayuuka@users.noreply.github.com>
2024-05-14 14:38:06 +0900

    Update Kato_Chapter18.java

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -5,23 +5,21 @@ public abstract class Kato_Chapter18 {
     public String givenName;
     public String address;
 
-    // コンストラクタで姓と住所を初期化
+    
     public Kato_Chapter18(String givenName) {
         this.familyName = "加藤";
         this.address = "東京都中野区〇×";
         this.givenName = givenName;
     }
 
-    // 共通の紹介を出力するメソッド
     public void commonIntroduce() {
         System.out.println("名前は " + familyName + givenName + " です");
         System.out.println("住所は " + address + " です");
     }
 
-    // 抽象メソッド（子クラスで実装）
+    
     public abstract void eachIntroduce();
 
-    // 紹介を実行するメソッド
     public void execIntroduce() {
         commonIntroduce();
         eachIntroduce();
