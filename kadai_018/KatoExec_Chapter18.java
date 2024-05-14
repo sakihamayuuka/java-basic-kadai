e590eba9020fa139f773082d1e06a8dcde4e713b
sakihamayuuka <166109381+sakihamayuuka@users.noreply.github.com>
2024-05-14 15:42:10 +0900

    Update KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -7,13 +7,16 @@ public class KatoExec_Chapter18{
         Kato_Chapter18 taro = new KatoTaro_Chapter18();
         Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
         Kato_Chapter18 hanako = new KatoHanako_Chapter18();
-
+ 
+        taro.setGivenName();
         taro.execIntroduce();
         System.out.println();
 
+        ichiro.setGivenName();
         ichiro.execIntroduce();
         System.out.println(); 
 
+        hanako.setGivenName();
         hanako.execIntroduce();
         System.out.println(); 
 	}
