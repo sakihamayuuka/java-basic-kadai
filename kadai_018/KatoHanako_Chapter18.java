d4bc5669d4037fc7249aed691e3ae949bb4597a4
sakihamayuuka <166109381+sakihamayuuka@users.noreply.github.com>
2024-05-14 15:41:53 +0900

    Update KatoHanako_Chapter18.java

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -2,13 +2,13 @@ package kadai_018;
 
 public class KatoHanako_Chapter18 extends Kato_Chapter18 {
 
-    public KatoHanako_Chapter18() {
-        super("花子");  // 親クラスのコンストラクタを呼び出してgivenNameをセット
+    public void setGivenName() {
+        givenName = "花子";
     }
 
+
     // 個別の紹介を出力するメソッド
-    @Override
-    public void eachIntroduce() {
+       public void eachIntroduce() {
         System.out.println("趣味は読書です");
     }
 }
